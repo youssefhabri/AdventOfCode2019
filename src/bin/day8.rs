e0b83f9a50ed34acad30624bbae5925f5d97d891
Youssef Habri <youssef.habri@gmail.com>
2019-12-08 10:51:29 +0100

    Add day 8 part 2

--- src/bin/day8.rs
+++ src/bin/day8.rs
@@ -1,10 +1,10 @@
 fn main() {
     let input = include_str!("../../inputs/day08.txt");
-    let width = 25;
-    let height = 6;
+    const WIDTH: usize = 25;
+    const HEIGHT: usize = 6;
 
-    let num_layers = input.chars().count() / (width * height);
-    let step = width * height;
+    let num_layers = input.chars().count() / (WIDTH * HEIGHT);
+    let step = WIDTH * HEIGHT;
     let mut layers = vec![];
     for i in (0..input.chars().count()).step_by(step) {
         let layer = input.trim().get(i..(i + step)).unwrap_or("");
@@ -13,30 +13,21 @@ fn main() {
         }
     }
 
-    let mut min_zeros = (width * height + 1) as i32;
-    let mut layer_idx = 0;
-    layers.iter().enumerate().for_each(|(idx, layer)| {
-        let num_zeros = layer
-            .chars()
-            .filter_map(|c| if c == '0' { Some(1) } else { None })
-            .sum::<i32>();
-        if num_zeros < min_zeros {
-            min_zeros = num_zeros;
-            layer_idx = idx;
-        }
+    // Generate the message
+    let mut new_image = ['2'; WIDTH * HEIGHT];
+    layers.iter().for_each(|layer| {
+        layer.chars().enumerate().for_each(|(i, c)| {
+            if new_image[i] == '2' {
+                new_image[i] = c;
+            }
+        })
     });
 
-    dbg!(&min_zeros);
-    dbg!(&layer_idx);
-
-    let num_ones = layers[layer_idx]
-        .chars()
-        .filter_map(|c| if c == '1' { Some(1) } else { None })
-        .sum::<i32>();
-    let num_twos = layers[layer_idx]
-        .chars()
-        .filter_map(|c| if c == '2' { Some(1) } else { None })
-        .sum::<i32>();
-
-    println!("{}", num_ones * num_twos);
+    // Print the Message
+    for y in 0..HEIGHT {
+        for x in 0..WIDTH {
+            print!("{}", new_image[x + (y * WIDTH)]);
+        }
+        println!();
+    }
 }
